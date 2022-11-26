3c0caa4401f75afb0d61d677c39e595c1518bf21
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2022-11-26 02:09:36 -0300

    Add initial project skeleton

---
+++ Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+
+printfn "Hello from F#"
+
+let name = Console.ReadLine()
+let currentDate = DateTime.Now
+
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!")
+Console.Write($"{Environment.NewLine}Press any key to exit...")
+Console.ReadKey(true) |> ignore
