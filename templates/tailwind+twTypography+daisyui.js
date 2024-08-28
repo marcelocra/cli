1f1ba9543dafd28f2b6f51c890ce6c9c901d8344
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-08-27 23:53:23 -0300

    feat: add vue and svelte to tailwind main file

--- templates/tailwind+twTypography+daisyui.js
+++ templates/tailwind+twTypography+daisyui.js
@@ -3,7 +3,7 @@ import twTypography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,svelte,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
