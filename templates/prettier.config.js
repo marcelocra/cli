bce14d57cad55dc8d8bd655b31fb1dcf6bd4b104
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-11-30 13:47:43 -0300

    feat(prettier): add `~` to import sorter

--- templates/prettier.config.js
+++ templates/prettier.config.js
@@ -3,7 +3,7 @@
  * @type {import("prettier").Config}
  */
 const config = {
-  importOrder: ["^node:", "^npm:", "^jsr:", "<THIRD_PARTY_MODULES>", "^@/(.*)$", "^[./]", "^[../]"],
+  importOrder: ["^node:", "^npm:", "^jsr:", "<THIRD_PARTY_MODULES>", "^@/(.*)$", "^~/(.*)$", "^[./]", "^[../]"],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
   plugins: ["@trivago/prettier-plugin-sort-imports"],
