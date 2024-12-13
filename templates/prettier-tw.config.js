e99bf72b60e366bbc4976afc791eb24beeabe6fa
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-12-13 15:15:07 -0300

    feat: fix prettier-tw template and update prettier

--- templates/prettier-tw.config.js
+++ templates/prettier-tw.config.js
@@ -7,5 +7,6 @@ const config = {
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
   plugins: ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+};
 
 export default config;
